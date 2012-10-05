ae91c3f16e794db149b0a5f9b1dc4a49c5ef98e1
William Helme <william.helme@holidayextras.com>
2012-10-05 17:04:17 +0100

    added structure for functions that need to be implemented, added the exclusion of symlink and update the README file

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 RubyDoc
 =======
 
-Auto Readme generator for repositories 
+Auto Readme generator for repositories
+
+TODO
+=======
+Functions
+- filetype_ok (pull in list of filetypes that are ok from external txt file)
+- extract_comments (loop over the file and extract the comments, based on comment section style)
+- change is_excluded so that when it excludes by directory name pull in from file like the filetype_ok function
+ 
+
--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+# inclusions and global variable declarations
 require 'pathname'
 $inc_unix = false
 $root = ""
@@ -5,16 +6,26 @@ $root = ""
 #this exclusion by name can later be developed by bringing in values from a txt file and add them to the array
 $excluded = ['test','abc']
 
+#if the file/folder name meets a certian criteria we dont want, exclude it
 def is_excluded(val)
   case 
   when val =~ /^\./
     return true
   when $excluded.include?(val) 
     return true 
+  when File.symlink?(val)
+    return true
+  else
+    return false
   end
-  return false
 end
 
+#opposite of is_excluded, again can later be developed bringing in values from a txt file
+def filetype_ok()
+
+end
+
+#loop the current folder passed in via command line and print out valid files we can access
 def loop_cur_folder(folder)
   Dir.entries(folder).each do|cur_obj|
     case
@@ -26,12 +37,19 @@ def loop_cur_folder(folder)
         loop_cur_folder(cur_obj)
       else
         #check file and pull comment section out when readme tag found
+        # extract_comments(cur_obj)
         puts "  File found: " + cur_obj
       end
     end
   end
 end
 
+#extract comments from the file
+def extract_comments(file)
+
+end
+
+#Pull in command line arguments and run the program (initializer)
 ARGV.each do |var|
   case
     when  var == 'inc_unix' 
