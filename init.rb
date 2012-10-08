100866260db159b9ac6ab269f51b923ee5f01c2d
Paul Clarke <pauly@clarkeology.com>
2012-10-08 16:33:08 +0200

    Update init.rb
    make it so we can chain methods on the file name instead. 
    Still a work in progress, still improving my ruby.

--- init.rb
+++ init.rb
@@ -1,61 +1,62 @@
+#!/usr/bin/ruby
+
 # inclusions and global variable declarations
 require 'pathname'
 $inc_unix = false
-$root = ""
 
-#this exclusion by name can later be developed by bringing in values from a txt file and add them to the array
-$excluded = ['test','abc']
+#!/usr/bin/ruby
 
-#if the file/folder name meets a certian criteria we dont want, exclude it
-def is_excluded(val)
-  case 
-  when val =~ /^\./
-    return true
-  when $excluded.include?(val) 
-    return true 
-  when File.symlink?(val)
-    return true
-  else
+# inclusions and global variable declarations
+require 'pathname'
+$inc_unix = false
+
+class String # monkey patch the string class
+  def is_excluded?
+    #if the file/folder name meets a certian criteria we dont want, exclude it
+    #this exclusion by name can later be developed by bringing in values from a txt file and add them to the array
+    $excluded = ['test','abc']
+    if self =~ /^\./
+      return true
+    elsif File.symlink? self
+      return true
+    elseif $excluded.include? self
+      return true
+    end
     return false
+  end
+  def directory?
+    return File.directory? self
+  end
+  def ok?
+    return ! self.is_excluded?
+  end
+  def extract_comments
+    # get file contents, extract stuff for the readme
   end
 end
 
-#opposite of is_excluded, again can later be developed bringing in values from a txt file
-def filetype_ok()
-
-end
-
 #loop the current folder passed in via command line and print out valid files we can access
-def loop_cur_folder(folder)
-  Dir.entries(folder).each do|cur_obj|
-    case
-    when is_excluded(cur_obj)
-      #do nothing
-    else
-      if File.directory?(cur_obj) == true
-        puts "Iterating over: " + cur_obj
-        loop_cur_folder(cur_obj)
+def loop_cur_folder folder
+  Dir.entries(folder).each do | f |
+    if f.ok?
+      if f.directory?
+        puts "Iterating over: " + f
+        loop_cur_folder f
       else
-        #check file and pull comment section out when readme tag found
-        # extract_comments(cur_obj)
-        puts "  File found: " + cur_obj
+        # check file and pull comment section out when readme tag found
+        puts "  File found: " + f
+        # f.extract_comments
       end
     end
   end
 end
 
-#extract comments from the file
-def extract_comments(file)
-
-end
-
 #Pull in command line arguments and run the program (initializer)
 ARGV.each do |var|
   case
-    when  var == 'inc_unix' 
+    when  var == 'inc_unix'
       $inc_unix = true
     else
-      $root = var
       puts "Iterating over: " + var
       loop_cur_folder(var)
   end
